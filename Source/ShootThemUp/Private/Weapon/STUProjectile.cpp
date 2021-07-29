5eae489ccb554dc0b0786d92ca6052cac6ab0f47
Artem Kopeykin <artemko.one@gmail.com>
2021-07-30 00:55:42 +0300

    55. Гранатомёт 2: движение ракеты

--- Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
+++ Source/ShootThemUp/Private/Weapon/STUProjectile.cpp
@@ -3,6 +3,7 @@
 
 #include "Weapon/STUProjectile.h"
 #include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 ASTUProjectile::ASTUProjectile()
 {
@@ -11,9 +12,17 @@ ASTUProjectile::ASTUProjectile()
 	CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
 	CollisionComponent->SetSphereRadius(5.f);
 	SetRootComponent(CollisionComponent);
+
+	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
+	MovementComponent->InitialSpeed = 1500.f;
+	MovementComponent->ProjectileGravityScale = 0.f;
 }
 
 void ASTUProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+
+	check(MovementComponent);
+	MovementComponent->Velocity = ShotDirection * MovementComponent->InitialSpeed;
+	SetLifeSpan(5.f);
 }
