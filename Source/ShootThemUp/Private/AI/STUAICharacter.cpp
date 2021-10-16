c7b10835059fd829786eb1a12da14a3d1525f9a5
Artem Kopeykin <artemko.one@gmail.com>
2021-10-16 16:40:21 +0300

    93. AI Move To. NavMesh Volume

---
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -0,0 +1,11 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "AI/STUAICharacter.h"
+#include "AI/STUAIController.h"
+
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	AIControllerClass = ASTUAIController::StaticClass();
+}
