07f524248215f3c1e53abfec0f99f82ea53c57fd
Artem Kopeykin <artemko.one@gmail.com>
2021-11-04 22:08:21 +0300

    115. Стрельба только по игроку из другой команды

--- Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
@@ -4,6 +4,7 @@
 #include "Components/STUAIPerceptionComponent.h"
 #include "AIController.h"
 #include "STUHealthComponent.h"
+#include "STUUtils.h"
 #include "Perception/AISense_Sight.h"
 
 AActor* USTUAIPerceptionComponent::GetClosestEnemy() const
@@ -24,7 +25,11 @@ AActor* USTUAIPerceptionComponent::GetClosestEnemy() const
 	for (const auto PerceiveActor : PerceiveActors)
 	{
 		const auto HealthComponent = PerceiveActor->FindComponentByClass<USTUHealthComponent>();
-		if (HealthComponent && !HealthComponent->IsDead()) //todo check if enemy or not
+
+		const auto PerceivePawn = Cast<APawn>(PerceiveActor);
+		const auto AreEnemies = PerceivePawn && FSTUUtils::AreEnemies(Controller, PerceivePawn->GetController());
+		
+		if (HealthComponent && !HealthComponent->IsDead() && AreEnemies)
 		{
 			const auto CurrentDistance = FVector::Distance(PerceiveActor->GetActorLocation(), Pawn->GetActorLocation());
 			if (CurrentDistance < BestDistance)
--- Source/ShootThemUp/Public/STUUtils.h
+++ Source/ShootThemUp/Public/STUUtils.h
@@ -1,5 +1,17 @@
 ﻿#pragma once
+#include "STUPlayerState.h"
 
 class FSTUUtils
 {
+public:
+	bool static AreEnemies(AController* Controller1, AController* Controller2)
+	{
+		if (!Controller1 || !Controller2 || Controller1 == Controller2) return false;
+
+		const auto PlayerState1 = Controller1->GetPlayerState<ASTUPlayerState>();
+		const auto PlayerState2 = Controller2->GetPlayerState<ASTUPlayerState>();
+		if (!PlayerState1 || !PlayerState2) return false;
+
+		return PlayerState1->GetTeamID() != PlayerState2->GetTeamID();
+	}
 };
